77d51e24d37a152be935915243fb217263127157
sbretz3 <sbretz3@gatech.edu>
2016-09-19 14:40:29 -0400

    update

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,7 @@ Rails.application.routes.draw do
 
   get 'auth0/failure'
 
-  get 'auth0/home' => "auth0#home"
-  get 'auth0/loggedinas'
+  get 'static_pages#home'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
